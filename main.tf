3459574256b4b136943c21e703b586f8a633213a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-18 13:30:48 -0400

    Switch node type

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "openstack" {
   instances = {
     mgmt   = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
     login  = { type = "p2-3gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "g1-18gb-c4-22gb", tags = ["node"], count = var.node_count }
+    node   = { type = "p2-3gb", tags = ["node"], count = var.node_count }
   }
 
   volumes = {
